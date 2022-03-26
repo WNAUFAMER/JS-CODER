fbba3d1dd93eeb6fe70a74cf125b9cb56c39f1f6
William Naufamer <williamnaufamer@gmail.com>
2022-03-26 20:02:33 -0300

    Entregable1 Naufamer

---
+++ funciones.js
@@ -0,0 +1,20 @@
+const cuotaM = parseInt(1000); 
+const cuotaJ = parseInt(1300);   
+const cuotaA = parseInt(1700);     ;
+
+for (let i= 0; i<5; i++) {
+    let nombre = prompt("Ingrese nombre");
+    let apellido = prompt("Ingrese apellido");
+    let edadIngresada = parseInt(prompt("ingrese edad"));
+    let completo = `${nombre} ${apellido}`;
+
+    if (edadIngresada<=12 && edadIngresada>0)
+        alert(`Estimado/a ${completo} usted tiene ${edadIngresada} años recibe la caterigoria MENOR y el valor de la cuota es${cuotaM} pesos argentinos`);
+
+    else if (edadIngresada>13 && edadIngresada<18) {
+        alert(`Estimado/a ${completo} usted tiene ${edadIngresada} años recibe la caterigoria JUVENIL y el valor de la cuota es${cuotaJ}pesos argentinos`);
+
+    } else {
+        alert(`Estimado/a ${completo} usted tiene ${edadIngresada} años recibe la caterigoria ADULTO y el valor de la cuota es${cuotaA} pesos argentinos`);
+    }
+};
