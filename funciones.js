ae9d3eac2aacc81d49fe99c87fe11c89006f51eb
William Naufamer <williamnaufamer@gmail.com>
2022-04-16 11:09:10 -0300

    primera entrega

--- funciones.js
+++ funciones.js
@@ -1,22 +1,39 @@
-//Cree un algoritmo que haga le pide al usuario sus datos ,le asigna categoria segun su edad y valor de cuota social 
 
-const cuotaM = parseInt(1000); 
-const cuotaJ = parseInt(1300);   
-const cuotaA = parseInt(1700);     ;
-
-for (let i= 0; i<5; i++) {
-    let nombre = prompt("Ingrese nombre");
-    let apellido = prompt("Ingrese apellido");
-    let edadIngresada = parseInt(prompt("ingrese edad"));
-    let completo = `${nombre} ${apellido}`;
-
-    if (edadIngresada<=12 && edadIngresada>0)
-        alert(`Estimado/a ${completo} usted tiene ${edadIngresada} años recibe la caterigoria MENOR y el valor de la cuota es  ${cuotaM}  pesos argentinos`);
-
-    else if (edadIngresada>13 && edadIngresada<18) {
-        alert(`Estimado/a ${completo} usted tiene ${edadIngresada} años recibe la caterigoria JUVENIL y el valor de la cuota es  ${cuotaJ}  pesos argentinos`);
-
-    } else {
-        alert(`Estimado/a ${completo} usted tiene ${edadIngresada} años recibe la caterigoria ADULTO y el valor de la cuota es  ${cuotaA}  pesos argentinos`);
+class Carrito {
+    constructor(clave) {
+        this.clave = clave || "productos";
+        this.productos = this.obtener();
     }
-};
+
+    agregar(producto) {
+        if (!this.existe(producto.id)) {
+            this.productos.push(producto);
+            this.guardar();
+        }
+    }
+
+    quitar(id) {
+        const indice = this.productos.findIndex(p => p.id === id);
+        if (indice != -1) {
+            this.productos.splice(indice, 1);
+            this.guardar();
+        }
+    }
+
+    guardar() {
+        localStorage.setItem(this.clave, JSON.stringify(this.productos));
+    }
+
+    obtener() {
+        const productosCodificados = localStorage.getItem(this.clave);
+        return JSON.parse(productosCodificados) || [];
+    }
+
+    existe(id) {
+        return this.productos.find(producto => producto.id === id);
+    }
+
+    obtenerConteo() {
+        return this.productos.length;
+    }
+}
